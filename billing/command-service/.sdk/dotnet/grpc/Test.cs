82e6f5f66234b1bda502cc293db41b0be9e43219
Jeevan James <jeevanjj@live.com>
2021-09-15 12:26:24 +0530

    Add gRPC client SDK

---
+++ billing/command-service/.sdk/dotnet/grpc/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Net.Client;
+
+namespace JeevanInc.Billing.CommandService.Client.Grpc
+{
+    public class Test
+    {
+        public async Task DoSomething()
+        {
+            using var channel = new GrpcChannel("https://localhost:5001");
+            var client = new BillingService.BillingServiceClient(channel);
+            await client.BillCustomerAsync()
+        }
+    }
+}
